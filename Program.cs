c032288592d8230bd6a813a055ef9d90fd53dc05
swatishinde09 <148969831+swatishinde09@users.noreply.github.com>
2023-11-29 14:46:48 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinPipeline_ConsolApp1
+{
+    internal class Program
+    {
+        void dispaly()
+        {
+            int a=10, b=20, c;
+            c = a + b;
+            Console.WriteLine(c);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.dispaly();
+            Console.ReadLine();
+        }
+    }
+}
